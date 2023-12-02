98259ad756d011081049ef7f168126cc70d7cf59
defcon4017 <152660034+defcon4017@users.noreply.github.com>
2023-12-01 23:28:49 -0600

    Update uber_data.py

--- uber_data.py
+++ uber_data.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+from streamlit_extras.metric_cards import style_metric_cards
+
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 st.sidebar.header('Dashboard - Neoteric.AI')
 
