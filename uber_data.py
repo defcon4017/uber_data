f8afa8bb9926a1b978339b3063593bfba7282b2d
defcon4017 <152660034+defcon4017@users.noreply.github.com>
2023-12-01 22:08:45 -0600

    Update uber_data.py

--- uber_data.py
+++ uber_data.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.set_page_config(layout='wide', initial_sidebar_state='expanded')           
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+st.sidebar.header('Dashboard - Neoteric.AI')
 
 st.title('Uber pickups in NYC')
 
